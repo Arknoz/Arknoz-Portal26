176d8186d8f9d4cd6d56331c4220780b61cb983a
Arknoz <info@Arknoz.com>
2026-09-12 04:26:51 +0530

    M12 final continent geography master

--- apps/web/src/app/global/[...path]/page.tsx
+++ apps/web/src/app/global/[...path]/page.tsx
@@ -1,20 +1,46 @@
 ﻿import { notFound } from "next/navigation";
+
 import GeographyPage from "@/components/geography/GeographyPage";
-import { findGeography } from "@/lib/geography";
+import ContinentGeographyPage from "@/components/geography/ContinentGeographyPage";
+
+import {
+  findGeography,
+} from "@/lib/geography";
+
 
 export default async function Page({
   params,
 }: {
-  params: Promise<{ path: string[] }>;
+  params: Promise<{
+    path: string[];
+  }>;
 }) {
   const { path } = await params;
 
-  const slug = path[path.length - 1];
-  const context = findGeography(slug);
+  const slug =
+    path[path.length - 1];
+
+  const context =
+    findGeography(slug);
 
   if (!context) {
     notFound();
   }
 
-  return <GeographyPage context={context} />;
+  if (
+    context.type ===
+    "continent"
+  ) {
+    return (
+      <ContinentGeographyPage
+        context={context}
+      />
+    );
+  }
+
+  return (
+    <GeographyPage
+      context={context}
+    />
+  );
 }
